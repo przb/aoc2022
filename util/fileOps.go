7184a848819ed37a576863ddfc4c682ba6d0e4dd
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2022-12-18 11:38:10 -0600

    add get tokens function

--- util/fileOps.go
+++ util/fileOps.go
@@ -3,6 +3,7 @@ package util
 import (
 	"bufio"
 	"os"
+	"strings"
 )
 
 func ReadLines(filename string) ([]string, error) {
@@ -23,6 +24,30 @@ func ReadLines(filename string) ([]string, error) {
 
 	for scn.Scan() {
 		lines = append(lines, scn.Text())
+	}
+	return lines, nil
+}
+
+// ReadTokens returns a 2-d array for the file with the given filename
+func ReadTokens(filename string) ([][]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	stat, err := file.Stat()
+
+	if err != nil {
+		return nil, err
+	}
+	lines := make([][]string, 0, stat.Size())
+
+	scn := bufio.NewScanner(file)
+
+	for scn.Scan() {
+		line := strings.Split(scn.Text(), " ")
+		lines = append(lines, line)
 	}
 	return lines, nil
 }
