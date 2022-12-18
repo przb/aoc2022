09a21503d3f01634073150bd64a9994aa1351e02
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2022-12-18 12:00:41 -0600

    add win caclulator

--- day02/rpc.go
+++ day02/rpc.go
@@ -4,7 +4,20 @@ import (
 	"github.com/przb/aoc2022/util"
 )
 
-const filename = "inputs/input02-1.txt"
+const (
+	filename = "inputs/input02-1.txt"
+
+	losePts = 0
+	tiePts  = 3
+	winPts  = 6
+
+	opponentRock     = "A"
+	opponentPaper    = "B"
+	opponentScissors = "C"
+	myRock           = "X"
+	myPaper          = "Y"
+	myScissors       = "Z"
+)
 
 // Returns the sum of the points you get from choosing rock, paper, or scissors
 func selectionSum() int {
@@ -18,6 +31,38 @@ func selectionSum() int {
 
 	for _, line := range lines {
 		sum += m[line[1]]
+	}
+
+	return sum
+}
+
+func win(opponentSel, mySel string) bool {
+	return (opponentSel == opponentRock && mySel == myPaper) ||
+		(opponentSel == opponentPaper && mySel == myScissors) ||
+		(opponentSel == opponentScissors && mySel == myRock)
+}
+
+// Returns the points you get from the rounds if you win
+func winSum() int {
+
+	// if equal -> 3 pts
+	// if won -> 6 pts
+	// if lost -> 0 pts
+	sum := 0
+
+	lines, err := util.ReadTokens(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, line := range lines {
+		if line[0] == line[1] {
+			sum += tiePts
+		} else if win(line[0], line[1]) {
+			sum += winPts
+		} else {
+			sum += losePts
+		}
 	}
 
 	return sum
