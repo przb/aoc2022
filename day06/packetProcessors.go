021440cb23b8c064e27ca9f94060594dd153dc26
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2022-12-23 17:55:51 -0600

    solve day06 part 1

---
+++ day06/packetProcessors.go
@@ -0,0 +1,48 @@
+package day06
+
+import (
+	"fmt"
+	"github.com/przb/aoc2022/util"
+	"strings"
+)
+
+const filename = "inputs/input06.txt"
+
+func uniqueInString(s []string, sub string) bool {
+	found := false
+	for _, l := range s {
+		if l == sub && found == true {
+			return false
+		} else if l == sub {
+			found = true
+		}
+	}
+	return true
+}
+func noDuplicates(s []string) bool {
+	for _, l := range s {
+		if !uniqueInString(s, l) {
+			return false
+		}
+	}
+	return true
+}
+
+func findIndexOfFirstUniqueString(s []string) int {
+	for i := 4; i < len(s); i++ {
+		substr := s[i-4 : i]
+		if noDuplicates(substr) {
+			return i
+		}
+	}
+	return -1
+}
+
+func NumCharacters() {
+	lines, err := util.ReadLines(filename)
+	if err != nil {
+		panic(err)
+	}
+	characters := strings.Split(lines[0], "") // there is only one line for this puzzle
+	fmt.Println(findIndexOfFirstUniqueString(characters))
+}
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/przb/aoc2022/day05"
+	"github.com/przb/aoc2022/day06"
 )
 
 func main() {
-	day05.TopCrates()
+	day06.NumCharacters()
 }
